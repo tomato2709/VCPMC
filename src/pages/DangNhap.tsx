50727bce7d269c4dbb9ebe87a774b399c4319e41
tomato2709 <mctomato2709@gmail.com>
2023-05-03 23:00:45 +0700

    added login page

---
+++ src/pages/DangNhap.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../components/login/login.css';
+import logo from '../logo.svg';
+import Login from "../components/login/login";
+
+const DangNhap: React.FC = () => {
+    return (
+        <Login />
+    );
+}
+
+export default DangNhap;
---
+++ src/pages/KhoBanGhi.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Menu from "../components/menu/menu";
+
+const KhoBanGhi: React.FC = () => {
+    return (
+        <div>
+            <Menu />
+        </div>
+    );
+}
+
+export default KhoBanGhi;
